7e7f661b5ccb76d70d334c8346a0f7bdfaa9ddad
jeovanedossantossantos <jeovane.ssantos@outlook.com>
2022-10-19 15:07:26 -0300

    chore: game mine

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Field from './components/Campo/Field';
+import params from "./params"
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Iniciando o Mines!!!</Text>
+      <Text>Tamano da grade:
+        {params.getRowsAmount()}X{params.getColumnsAmount()}
+      </Text>
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={6} />
+      <Field mined />
+      <Field mined opened />
+      <Field mined opened exploded />
+      <Field flagged />
+      <Field flagged opened />
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
